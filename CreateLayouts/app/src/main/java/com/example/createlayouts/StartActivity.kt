f10bf606a1e6cca35df167f1f975667b41b8da02
smyy22 <96207103+smyy22@users.noreply.github.com>
2022-04-28 19:08:41 +0300

    dataBinding added

--- CreateLayouts/app/src/main/java/com/example/createlayouts/StartActivity.kt
+++ CreateLayouts/app/src/main/java/com/example/createlayouts/StartActivity.kt
@@ -1,13 +1,25 @@
 package com.example.createlayouts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.createlayouts.databinding.ActivityStartBinding
 
-class StartActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding:ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_start)
+        binding.button.setOnClickListener(this)
 
+
+    }
+
+    override fun onClick(p0: View?) {
+        val intent:Intent=Intent(this,PremiumActivity::class.java)
+        startActivity(intent)
     }
 
 
